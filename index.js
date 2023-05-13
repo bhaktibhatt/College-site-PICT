4006517a17fea4bcdeb874cb09032f6b33b16a62
Bhakti <bhaktinirbhaybhatt@gmail.com>
2023-05-13 11:33:12 +0530

    carousel working

--- index.js
+++ index.js
@@ -1,7 +1,37 @@
+currentSlideID = 1;
+sliderElement = document.getElementById('slider');
+totalSlides = sliderElement.childElementCount;
+console.log(totalSlides)
 
-// import {
-//     Carousel,
-//     initTE,
-//   } from "tw-elements";
-  
-//   initTE({ Carousel });
+
+
+function next(){
+    if(currentSlideID < totalSlides)
+    {
+        currentSlideID++;
+        showSlide();
+
+    }
+}
+function prev(){
+    if(currentSlideID>1)
+    {
+        currentSlideID--;
+        showSlide();
+    }
+
+}
+function showSlide()
+{
+    slides = document.getElementById('slider').getElementsByTagName('li');
+    for (let index = 0; index < totalSlides; index++) {
+        const element = slides[index];
+        if(currentSlideID === index+1){
+            element.classList.remove('hidden')
+        }
+        else{
+            element.classList.add('hidden')
+        }
+        
+    }
+}
